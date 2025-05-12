6c051a5ee6d2bdac8bcb528488a4fc8ca2eee3ea
Mohamed Wael <103073614+MohamedWael3011@users.noreply.github.com>
2025-05-12 18:38:41 +0300

    Added Transaction Stuff

--- services/user-service/src/routes/authRoutes.js
+++ services/user-service/src/routes/authRoutes.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/authController');
-const { validate } = require('../middlewares/validate');
-const { loginValidation, registerValidation } = require('../validations/authValidation');
+const authController = require("../controllers/authController");
+const { validate } = require("../middlewares/validate");
+const {
+  loginValidation,
+  registerValidation,
+} = require("../validations/authValidation");
 
-router.post('/register', validate(registerValidation), authController.register);
-router.post('/login', validate(loginValidation), authController.login);
+router.post("/register", validate(registerValidation), authController.register);
+router.post("/login", validate(loginValidation), authController.login);
 
 module.exports = router;
